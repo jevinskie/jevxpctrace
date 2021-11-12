e834b21f1c6f4090c255f7ed7037e249a6369e82
Jevin Sweval <jevinsweval@gmail.com>
2021-11-12 15:41:25 -0800

    wip

--- jevxpctrace-test/jevxpctrace-test-client/ViewController.m
+++ jevxpctrace-test/jevxpctrace-test-client/ViewController.m
@@ -9,7 +9,47 @@
 
 #import <jevxpctrace-test-service/jevxpctrace_test_serviceProtocol.h>
 
+#include <CaptainHook.h>
+
 BOOL doBP;
+
+CHDeclareClass(NSXPCDecoder);
+
+@interface NSXPCDecoder : NSXPCCoder
+- (int)__decodeXPCObject:(xpc_object_t)root allowingSimpleMessageSend:(BOOL)allowSimpleMessageSend outInvocation:(NSInvocation **)invocation outArguments:(id *)arguments outArgumentsMaxCount:(NSUInteger)maxArgCount outMethodSignature:(NSMethodSignature **)outMethodSignature outSelector:(SEL *)outSelector isReply:(BOOL)isReply replySelector:(SEL)replySelector interface:(NSXPCInterface *)interface;
+@end
+
+CHConstructor {
+    CHLoadLateClass(NSXPCDecoder);
+}
+
+
+@protocol DummyProtocol
+@end
+
+void dumpXPCObject(xpc_object_t dict) {
+//    doBP = NO;
+    int res = -1;
+    NSInvocation *invoc;
+    __autoreleasing id args[32] = {nil};
+    NSMethodSignature *sig;
+    SEL sel;
+
+    NSLog(@"dict: %@", dict);
+
+    xpc_object_t root = xpc_dictionary_get_value(dict, "root");
+    NSLog(@"root: %@", root);
+
+    const void *root_buf = xpc_data_get_bytes_ptr(root);
+    size_t root_len = xpc_data_get_length(root);
+    NSLog(@"root_buf: %p root_len: %zu", root_buf, root_len);
+
+    NSXPCInterface *interface = [NSXPCInterface interfaceWithProtocol:@protocol(DummyProtocol)];
+
+    NSXPCDecoder *decoder = [[CHClass(NSXPCDecoder) alloc] init];
+    res = [decoder __decodeXPCObject:dict allowingSimpleMessageSend:YES outInvocation:&invoc outArguments:args outArgumentsMaxCount:32 outMethodSignature:&sig outSelector:&sel isReply:NO replySelector:nil interface:interface];
+    NSLog(@"res: %d invoc: %@ args[0]: %@ args[1]: %@ args[2]: %@ args[3]: %@ sig: %@ sel: %@", res, invoc, args[0], args[1], args[2], args[3], sig, NSStringFromSelector(sel));
+}
 
 @implementation ViewController
 
