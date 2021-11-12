2a3dce54aae1cc910c6932f88641b3b3fb998483
Jevin Sweval <jevinsweval@gmail.com>
2021-11-12 10:11:17 -0800

    jevxpctrace-test

---
+++ jevxpctrace-test/jevxpctrace-test-client/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jevxpctrace-test-client
+//
+//  Created by Jevin Sweval on 11/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
